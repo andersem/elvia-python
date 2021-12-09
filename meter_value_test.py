c50b496b0e4505cbc5ac7c3e45b5a491444ab932
Anders Emil Salvesen <andersems@gmail.com>
2021-12-09 20:57:10 +0100

    Rewrite to async

--- meter_value_test.py
+++ meter_value_test.py
@@ -1,3 +1,4 @@
+import asyncio
 from elvia import Elvia
 import json
 import os
@@ -7,23 +8,27 @@ token = os.environ.get("ELVIA_TOKEN")
 elvia = Elvia(token)
 meter_value_client = elvia.meter_value()
 
-print(
-    json.dumps(
-        meter_value_client.get_meter_values(
-            {
-                "start_time": "2021-12-08T01:00:00",
-                "end_time": "2021-12-08T02:00:00",
-                "include_production": True,
-            }
+async def print_max_hours():
+    print(
+        json.dumps(
+            await meter_value_client.get_meter_values(
+                {
+                    "start_time": "2021-12-08T01:00:00",
+                    "end_time": "2021-12-08T02:00:00",
+                    "include_production": True,
+                }
+            )
         )
     )
-)
 
-max_hours = meter_value_client.get_max_hours(
-    {
-        "calculate_time": "2021-12-08T01:00:00",
-        "include_production": True,
-    }
-)
 
-print(json.dumps(max_hours["meteringpoints"]))
+    max_hours = await meter_value_client.get_max_hours(
+        {
+            "calculate_time": "2021-12-08T01:00:00",
+            "include_production": True,
+        }
+    )
+
+    print(json.dumps(max_hours["meteringpoints"]))
+
+asyncio.run(print_max_hours())
